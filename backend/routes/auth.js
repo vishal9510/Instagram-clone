3155207ca0247733e7c65217526c4f0f059bd0e4
vishal <vishalsavaliya2912@gmail.com>
2024-12-28 04:14:37 +0530

    sign api and fetch

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -35,11 +35,33 @@ router.post("/signup", (req, res) => {
         })
     })
 
-
-
-
 })
 
+router.post("/signin", (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res.status(422).json({ error: "Please add email and password" })
+    }
+    USER.findOne({ email: email }).then((savedUser) => {
+        if (!savedUser) {
+            return res.status(422).json({ error: "Invalid email" })
+        }
+        bcrypt.compare(password, savedUser.password).then((match) => {
+            if (match) {
+                // return res.status(200).json({ message: "Signed in Successfully" })
+                const token = jwt.sign({ _id: savedUser.id }, Jwt_secret)
+                const { _id, name, email, userName } = savedUser
+
+                res.json({ token, user: { _id, name, email, userName } })
+
+                console.log({ token, user: { _id, name, email, userName } })
+            } else {
+                return res.status(422).json({ error: "Invalid password" })
+            }
+        })
+            .catch(err => console.log(err))
+    })
+})
 
 module.exports = router;
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -2,9 +2,56 @@ import React, { useState, useContext } from "react";
 import "./SignIn.css";
 import logo from "../img/logo.png";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { LoginContext } from "../context/LoginContext";
 
 
 export default function SignIn() {
+
+  const { setUserLogin } = useContext(LoginContext)
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  // Toast functions
+  const notifyA = (msg) => toast.error(msg)
+  const notifyB = (msg) => toast.success(msg)
+
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  const postData = () => {
+    //checking email
+    if (!emailRegex.test(email)) {
+      notifyA("Invalid email")
+      return
+    }
+    // Sending data to server
+    fetch("http://localhost:5000/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+
+      })
+    }).then(res => res.json())
+      .then(data => {
+        if (data.error) {
+          notifyA(data.error)
+        } else {
+          notifyB("Signed In Successfully")
+          console.log(data)
+          localStorage.setItem("jwt", data.token)
+          localStorage.setItem("user", JSON.stringify(data.user))
+
+          setUserLogin(true)
+          navigate("/")
+        }
+        console.log(data)
+      })
+  }
 
   return (
     <div className="signIn">
