52cf402b46e91422dbfefe88c433e2725bbc3cde
vishal <vishalsavaliya2912@gmail.com>
2024-12-28 22:23:46 +0530

    createpost api intigrate

--- frontend/src/components/Createpost.js
+++ frontend/src/components/Createpost.js
@@ -6,8 +6,73 @@ import { useNavigate } from "react-router-dom";
 
 
 
+export default function Createpost() {
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("")
+  const [url, setUrl] = useState("")
+  const navigate = useNavigate()
+
+  // Toast functions
+  const notifyA = (msg) => toast.error(msg)
+  const notifyB = (msg) => toast.success(msg)
+
+
   useEffect(() => {
 
+    // saving post to mongodb
+    if (url) {
+
+      fetch("http://localhost:5000/createPost", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+        body: JSON.stringify({
+          body,
+          pic: url
+        })
+      }).then(res => res.json())
+        .then(data => {
+          if (data.error) {
+            notifyA(data.error)
+          } else {
+            notifyB("Successfully Posted")
+            navigate("/")
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+  }, [url])
+
+
+  // posting image to cloudinary
+  const postDetails = () => {
+
+    console.log(body, image)
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "insta-clone")
+    data.append("cloud_name", "cantacloud2")
+    fetch("https://api.cloudinary.com/v1_1/cantacloud2/image/upload", {
+      method: "post",
+      body: data
+    }).then(res => res.json())
+      .then(data => setUrl(data.url))
+      .catch(err => console.log(err))
+    console.log(url)
+
+  }
+
+
+  const loadfile = (event) => {
+    var output = document.getElementById("output");
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src); // free memory
+    };
+  };
   return (
     <div className="createPost">
       {/* //header */}
